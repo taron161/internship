f038c630f960fd8e02d29cbde67f0bde1f4396b1
Taron <taron38@yandex.ru>
2024-01-22 17:42:08 +0300

    добавляет шаблон в проект

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,27 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import {iosVhFix} from './utils/ios-vh-fix';
+import {Form} from './modules/form-validate/form';
+import {openMneu} from './modules/open-menu/open-menu';
 
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  openMneu();
+
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    const form = new Form();
+    window.form = form;
+    form.init();
+  });
+});
